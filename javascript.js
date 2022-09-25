d35520d2fb1d33b6d24b616344006b8b4283c9a1
one987 <joelbed@gmail.com>
2022-09-25 00:02:36 -0400

    fleshing out playRound function pseudocode

--- javascript.js
+++ javascript.js
@@ -15,4 +15,21 @@ function getBotChoice() {
 }
 
 //single round of rock paper scisors function()
+function playRound(playerSelection, computerSelection) {
+
+    //if playerSelection equals computerSelection return "Tie Game!"
+
+    //if playerSelection is Rock and computerSelection is Paper return "You lose! Paper beats Rock."
+
+    //if playerSelection is Rock and computerSelection is Scissors return "You win! Rock beats Scissors."
+
+    //if playerSelection is Paper and computerSelection is Rock return "You win! Paper beats Rock."
+
+    //if playerSelection is Paper and computerSelection is Scissors return "You Lose! Scissors beats Paper."
+
+    //if playerSelection is Scissors and computerSelection is Rock return "You lose! Rock beats Scissors."
+
+    //if playerSelection is Scissors and computerSelection is paper return "You Win! Scissors beats Paper."
+    
+}
 
