f3333117cd1361637f06d711073e2987477d33b2
one987 <joelbed@gmail.com>
2022-09-24 23:36:18 -0400

    added getBotChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+//computer choice function()
+function getBotChoice() {
+
+    let items = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
+
+    let botChoice = items[Math.floor(Math.random()*items.length)]; 
+
+    return botChoice;  
+}
+
+//single round of rock paper scisors function()
+
