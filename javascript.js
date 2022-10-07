5b74b9f1ca9b1fb868af86f758bbe1694a4fbd74
one987 <joelbed@gmail.com>
2022-10-07 01:17:15 -0400

    refactored playRound function

--- javascript.js
+++ javascript.js
@@ -4,54 +4,43 @@ let userScore = 0;
 let botScore = 0;
 let gameCount = 0;
 
-
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () =>  {
+    console.log("rock");
+    
+})
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () =>  {
+    console.log("paper");
+})
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', () =>  {
+    console.log("scissors");
+})
 
 function getBotChoice() {
 
-    const items = [
-        "Rock",
-        "Paper",
-        "Scissors"
-    ];
-
+    const items = ["Rock", "Paper", "Scissors"];
     let botChoice = items[Math.floor(Math.random() * items.length)];
-
     return botChoice;
 }
 
 //Single round of rock paper scisors function()
 function playRound(playerSelection, computerSelection) {
-
-
-    if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
+    
+    if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper" || 
+    playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors" ||
+    playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock" ) {
         botScore++;
         gameCount++;
-        return "You lose! Paper beats Rock.";
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
-    else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
+    else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors" ||
+    playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock" ||
+    playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper" ) {
         userScore++;
         gameCount++;
-        return "You win! Rock beats Scissors.";
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") {
-        userScore++;
-        gameCount++;
-        return "You win! Paper beats Rock.";
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors") {
-        botScore++;
-        gameCount++;
-        return "You Lose! Scissors beats Paper.";
-    }
-    else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") {
-        botScore++;
-        gameCount++;
-        return "You lose! Rock beats Scissors.";
-    }
-    else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
-        userScore++;
-        gameCount++;
-        return "You Win! Scissors beats Paper.";
+        return `You win! ${playerSelection} beats ${computerSelection}`;
     }
     else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         gameCount++;
@@ -59,15 +48,16 @@ function playRound(playerSelection, computerSelection) {
     }
     else {
         gameCount++;
-        return "Error! You have  to choose Rock, Paper or Scissors!"
+        return "Error! You have to choose Rock, Paper or Scissors!"
     }
 }
 
 // Game function:
-function game() {
+function playGame() {
+
     for (let i = 0; i < 5; i++) {
-        //let playerSelection = prompt("Rock, Paper or Scissors?", "Rock");
-        let computerSelection = getBotChoice();
+        let playerSelection = prompt("Rock, Paper or Scissors?", "Rock");
+        const computerSelection = getBotChoice();
         console.log("You:", playerSelection, "", "Bot:", computerSelection);
         console.log(playRound(playerSelection, computerSelection));
         console.log("Score:", "You:", userScore, "Bot:", botScore);
@@ -85,15 +75,19 @@ function finalScore() {
     else if (gameCount == 5 && userScore < botScore) {
         return "You lose the Game!";
     }
-
     else if (gameCount == 5 && userScore == botScore) {
         return "It's a tie Game!";
     }
-
     else {
         return "";  //test if removing this will yield a cleaner code
     }
 }
 
-game();
+playGame();
 
+//to do for refactor:
+//add event listener to buttons
+//console.log results of clicking buttons to test 
+//modify playerSelection to use buttons instead of prompt
+//???
+//profit
